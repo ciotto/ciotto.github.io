a84286aeaeded1f1266f6a41d5970328d6f98a60
ciotto <bianciotto@elipse.it>
2019-04-10 18:30:20 +0200

    add Reverse the Haier T32X robot

--- sites/ci8/urls.py
+++ sites/ci8/urls.py
@@ -3,4 +3,5 @@ from ci8 import views
 
 urlpatterns = [
     url(r'^index.html$', views.index),
+    url(r'^haier-t32x.html$', views.haier_t32x),
 ]
--- sites/ci8/views.py
+++ sites/ci8/views.py
@@ -1,5 +1,19 @@
+import os
+import requests
 from django.shortcuts import render_to_response
 from staticsites.decorators import staticview
+from markdown2 import Markdown
+
+
+markdowner = Markdown(extras=['tables', 'fenced-code-blocks'])
+
+
+def html_from_markdown_url(url):
+    base_url = os.path.dirname(url)
+
+    response = requests.get(url)
+    content = response.content.replace('](images', '](%s/images' % base_url)
+    return markdowner.convert(content)
 
 
 @staticview
@@ -7,3 +21,13 @@ def index(request):
     ctx = {'title': 'Christian Bianciotto'}
 
     return render_to_response('ci8/index.html', ctx)
+
+
+@staticview
+def haier_t32x(request):
+    ctx = {
+        'title': 'Reverse the Haier T32X robot',
+        'md': html_from_markdown_url('https://raw.githubusercontent.com/ciotto/teardown/master/haier-t32x/README.md'),
+    }
+
+    return render_to_response('ci8/md.html', ctx)
