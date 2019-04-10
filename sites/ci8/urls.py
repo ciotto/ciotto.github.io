c6a2833b56cad78d33bcc03bb072490696e9f904
Christian Bianciotto <mail@ci8.it>
2019-04-10 19:13:43 +0200

    Add DIGIPASS GO 6 and menu

--- sites/ci8/urls.py
+++ sites/ci8/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from ci8 import views
+from ci8.views import pages
 
 urlpatterns = [
-    url(r'^index.html$', views.index),
-    url(r'^haier-t32x.html$', views.haier_t32x),
+    url(r'^%s$' % pages[0][1], views.index),
+    url(r'^%s$' % pages[1][1], views.haier_t32x),
+    url(r'^%s$' % pages[2][1], views.digipass_go_6),
 ]
